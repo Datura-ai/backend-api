c0113f198fc2411b4877af8af8d0e2d0dfdf5e45
Giga Chkhikvadze <giga.chkhikvadze@gmail.com>
2023-11-28 19:15:31 +0400

    refactor: main app, header styles, components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import ChatPage from "./components/pages/chatPage/ChatPage";
+
+import "./App.css";
+
+import Layout from "./components/common/Layout";
+import ChatPage from "./components/pages/ChatPage";
 
 function App() {
   return (
-    <div>
+    <Layout>
       <ChatPage />
-    </div>
+    </Layout>
   );
 }
 
---
+++ web/src/components/common/Header/Header.tsx
@@ -0,0 +1,12 @@
+import styles from "./headerStyles.module.css";
+import logoSrc from "../../../assets/taotensor_logo.png";
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      <img className={styles.logo} src={logoSrc} alt="n/a" />
+    </div>
+  );
+};
+
+export default Header;
