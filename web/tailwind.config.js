74bbf53b75d766aa035d3984ffcf5d9f37181f80
no-ki <nolanit229@gmail.com>
2023-11-29 07:33:11 -0800

    feat: :art: add initial libraries and improve structure

---
+++ web/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      visibility: ['group-hover'],
+    },
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
