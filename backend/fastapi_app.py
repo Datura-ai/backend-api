069da5eeeecdf2b085c549609c5062d6f4da7190
Vladyslav Chyzhov <chyzhov.dev@gmail.com>
2023-11-29 20:28:06 +0200

    feat: integration wandb into backend, implement conversation chaining

--- backend/fastapi_app.py
+++ backend/fastapi_app.py
@@ -5,7 +5,12 @@ from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 
-from neurons.api import initialize, query_synapse_image, query_synapse_text
+from neurons.api import (
+    initialize,
+    query_synapse_image,
+    query_synapse_text,
+    get_recent_images,
+)
 
 app = FastAPI()
 app.add_middleware(
@@ -18,19 +23,36 @@ app.add_middleware(
 )
 
 _, _, subtensor, dendrite, metagraph = initialize()
+prompts = []
+
+
+@app.get("/image-gallery")
+async def get_image_gallery():
+    try:
+        image_urls = get_recent_images()
+    except Exception:
+        raise HTTPException(
+            status_code=500, detail="Can't generate images. Please try again"
+        )
+
+    return image_urls
 
 
 @app.post("/generate-image")
 async def get_image(prompt_dict: dict):
-    prompt = prompt_dict.get('prompt')
+    prompt = prompt_dict.get("prompt")
     if not prompt:
         raise HTTPException(status_code=400, detail="Prompt is required")
 
-    generated_images = asyncio.run(query_synapse_image(dendrite, metagraph, subtensor, prompt))
+    generated_images = asyncio.run(
+        query_synapse_image(dendrite, metagraph, subtensor, prompt)
+    )
     try:
         image_url = generated_images[0].deserialize().get("url")
     except Exception:
-        raise HTTPException(status_code=500, detail="Can't generate image. Please try again")
+        raise HTTPException(
+            status_code=500, detail="Can't generate image. Please try again"
+        )
     return image_url
 
 
@@ -38,11 +60,14 @@ async def get_image(prompt_dict: dict):
 async def get_text(request: Request):
     try:
         body = await request.json()
-        prompt = body.get('prompt')
+        prompt = body.get("prompt")
     except json.JSONDecodeError:
         raise HTTPException(status_code=500, detail="Cant decode JSON")
     if not prompt:
         raise HTTPException(status_code=400, detail="Prompt is required")
 
-    return StreamingResponse(query_synapse_text(dendrite, metagraph, subtensor, prompt), media_type='text/event-stream')
-
+    prompts.append(prompt)
+    return StreamingResponse(
+        query_synapse_text(dendrite, metagraph, subtensor, prompts),
+        media_type="text/event-stream",
+    )
--- backend/neurons/api.py
+++ backend/neurons/api.py
@@ -1,8 +1,26 @@
-import bittensor as bt
 import argparse
+import datetime
+from io import BytesIO
 import traceback
-import asyncio
+import uuid
+
+import requests
+from PIL import Image
+import wandb
+import bittensor as bt
+
 from template.protocol import StreamPrompting, ImageResponse
+
+
+api = wandb.Api()
+wandb.init(
+    name="validator-18",
+    anonymous="allow",
+    reinit=False,
+    project="synthetic-QA",
+    entity="cortex-t",
+)
+bt.logging.success('Started wandb run')
 
 
 def initialize():
@@ -18,36 +36,70 @@ def initialize():
     return config, wallet, subtensor, dendrite, metagraph
 
 
-# axons = [36, 80, 255]
-
 async def query_synapse_image(dendrite, metagraph, subtensor, prompt):
+    wandb_data = {
+        "prompts": {},
+        "responses": {},
+        "images": {},
+        "timestamps": {},
+    }
     try:
-        axon = metagraph.axons[87]  # 87, 91, 98
+        axon = metagraph.axons[87]
         engine = "dall-e-3"
         size = "1792x1024"
         quality = "standard"
         style = "vivid"
 
-        syn = ImageResponse(messages=prompt, engine=engine, size=size, quality=quality, style=style)
+        syn = ImageResponse(
+            messages=prompt, engine=engine, size=size, quality=quality, style=style
+        )
 
         async def main():
             responses = await dendrite([axon], syn, deserialize=False, timeout=50)
-            full_response = []  # Initialize full_response
+            full_response = []
             for resp in responses:
                 full_response.append(resp)
 
             return full_response
 
         full_response = await main()
-        return full_response
+        if full_response:
+            response = full_response[0]
+            completion = response.completion
+            if completion is not None:
+                uid = f"{datetime.datetime.now().isoformat()}-{str(uuid.uuid4())}"
+                bt.logging.info(f"UID {uid} response is {completion}")
+                image_url = completion["url"]
+
+                # Download the image and store it as a BytesIO object
+                image_response = requests.get(image_url)
+                image_bytes = BytesIO(image_response.content)
+                image = Image.open(image_bytes)
+
+                # Log the image to wandb
+                wandb_data["images"][uid] = {
+                    "image": wandb.Image(image, caption=f"{uid}"),
+                    "uid": uid,
+                    "created_at": datetime.datetime.now().isoformat(),
+                }
+                wandb_data["responses"][uid] = completion
+                wandb_data["timestamps"][uid] = datetime.datetime.now().isoformat()
+
+                wandb.log(wandb_data)
+                return full_response
+
     except Exception as e:
         bt.logging.error(f"General exception at step: {e}\n{traceback.format_exc()}")
 
 
-async def query_synapse_text(dendrite, metagraph, subtensor, prompt):
+async def query_synapse_text(dendrite, metagraph, subtensor, prompts):
     try:
         axon = metagraph.axons[87]
-        syn = StreamPrompting(messages=[{"role": "user", "content": prompt}], engine="gpt-4-1106-preview", seed=1234)
+        syn = StreamPrompting(
+            messages=[{"role": "user", "content": prompt} for prompt in prompts],
+            engine="gpt-4-1106-preview",
+            seed=1234,
+        )
 
         responses = await dendrite([axon], syn, deserialize=False, streaming=True)
         for resp in responses:
@@ -59,11 +111,27 @@ async def query_synapse_text(dendrite, metagraph, subtensor, prompt):
         bt.logging.error(f"General exception at step: {e}\n{traceback.format_exc()}")
 
 
-def main():
-    config, wallet, subtensor, dendrite, metagraph = initialize()
-    # asyncio.run(query_synapse_text(dendrite, metagraph, subtensor, "tell me a story"))
-    asyncio.run(query_synapse_image(dendrite, metagraph, subtensor, "dragon"))
+def get_recent_images(limit=12):
+    runs = api.runs("cortex-t/synthetic-QA")
+    recent_images = []
+    file_count = 0
+    for run in runs:
+        for file in run.files():
+            if file.name.endswith(".png") or file.name.endswith(".jpg"):
+                image_url = file.url
+                timestamp = datetime.datetime.now().isoformat()
+                unique_id = str(uuid.uuid4())
+                image_info = {
+                    "image_url": image_url,
+                    "timestamp": timestamp,
+                    "uuid": unique_id
+                }
+                recent_images.append(image_info)
+                file_count += 1
+                if file_count >= limit:
+                    break
 
+        if file_count >= limit:
+            break
 
-if __name__ == "__main__":
-    main()
+    return recent_images
